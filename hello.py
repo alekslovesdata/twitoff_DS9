7ada5b1184e13c08b8b80f6e93797585ce7ad9e4
alekslovesdata <alekslovesdata@users.noreply.github.com>
2019-12-16 09:57:44 -0800

    adding minimal flask app that says hello world

---
+++ hello.py
@@ -0,0 +1,13 @@
+"""Minimal flask app"""
+
+from flask import Flask
+
+#Make the application
+app = Flask(__name__)
+
+#Make the route
+@app.route("/")
+
+#Now define a function
+def hello:
+    return "Hello World!"
