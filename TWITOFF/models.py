4972351d75b1487fd24ab6db255b862ae2416120
alekslovesdata <alekslovesdata@users.noreply.github.com>
2019-12-16 10:39:36 -0800

    added models file

---
+++ TWITOFF/models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+DB = SQLAlchemy()
+
+class User(DB.Model):
+    """Twitter users that we analyze"""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    name = DB.Column(DB.String(15), nullable=False)
+
+class Tweet(DB.Model):
+    """Tweets we pull"""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(280)) 
