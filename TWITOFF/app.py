38c380a199060e2e043bbfe416d6771b5333352b
alekslovesdata <alekslovesdata@users.noreply.github.com>
2019-12-16 10:58:25 -0800

    end of day 1

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,13 +1,19 @@
 """Code for our app"""
 
 from flask import Flask
+from .models import DB
 
 #make our app factory
 
 def create_app():
     app = Flask(__name__)
 
+    #add config for database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    #have the database know about the app
+    DB.init_app(app)
+
     @app.route('/')
     def root():
         return 'Welcome to Twitoff!!'
-    return app 
+    return app
