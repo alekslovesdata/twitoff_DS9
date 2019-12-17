c1e4c63793475e48aab14f647b8a8b1987be4afc
alekslovesdata <alekslovesdata@users.noreply.github.com>
2019-12-17 09:39:29 -0800

    changing schema formatting

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -6,6 +6,9 @@ class User(DB.Model):
     """Twitter users that we analyze"""
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
 
 class Tweet(DB.Model):
     """Tweets we pull"""
@@ -13,3 +16,5 @@ class Tweet(DB.Model):
     text = DB.Column(DB.Unicode(300))
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.text) 
