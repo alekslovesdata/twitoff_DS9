0c64a3dc64190b34e1d2492f9c863741b8fcd172
alekslovesdata <alekslovesdata@users.noreply.github.com>
2019-12-18 09:43:55 -0800

    added function to add or update twitter users in twitter.py

--- TWITOFF/twitter.py
+++ TWITOFF/twitter.py
@@ -12,4 +12,29 @@ TWITTER_AUTH.set_access_token(config('TWITTER_ACCESS_TOKEN'),
 TWITTER = tweepy.API(TWITTER_AUTH)
 BASILICA = basilica.Connection(config('BASILICA_KEY'))
 
-#to do - add functions later 
+#add functions here
+
+def add_or_update_user(username):
+    """Add or update a user and their tweets, or else give error"""
+    try:
+        twitter_user=TWITTER.get_user(username)
+        db_user=(User.query.get(twitter_user.id) or
+        User(id=twitter.id, name=username))
+        DB.session.add(db_user)
+        tweets = twitter_user.timeline(count=200, exclude_replies=True,
+        include_rts=False, tweet_mode='extended',
+        since_id=db_user.newest_tweet_id)
+        if tweets:
+            db_user.newest_tweet_id = tweets[0].id
+        for tweet in tweets:
+            #Calculate embedding on the full tweet
+            embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
+            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:300],
+            embedding=embedding)
+            db_user.tweets.append(db_tweet)
+            DB.session.add(db_tweet)
+    except Exception as e:
+        print('Error processing {}: {}'.format(username,e))
+        raise e
+    else:
+        DB.session.commit()
